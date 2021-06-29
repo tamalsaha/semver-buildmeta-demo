60837d1b0a423f8e7852e56421d408e179e56c38
Tamal Saha <tamal@appscode.com>
2021-06-29 01:34:48 -0700

    added-all

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tamalsaha/semver-buildmeta-demo
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go install test ====")
+	fmt.Println("v0.0.3+v2021.06.30")
+}
